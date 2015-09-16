91e9fdfd4a38a54412b49b4ca591182fd8be8620
procrasti <procrasti@k5-stats.org>
2015-09-16 10:52:59 +0800

    Create CSV file... I think numeical sim is correct.

--- simdrain.py
+++ simdrain.py
@@ -2,19 +2,19 @@
 
 # Simulate value flowing from protected balance into available balance that drains via wealth tax.
 
-from math import log as ln, exp, e
+from math import log as ln, exp, e, sqrt
 
-Ta = 1 # The half life in time units of value in A... ie A(t+Ta) = A(t)/2
-Tb = 7 # The half life in time units for value in B... ie, B(t)
+Ta = 1.0 # The half life in time units of value in A... ie A(t+Ta) = A(t)/2
+Tb = 1.0 # The half life in time units for value in B... ie, B(t)
 
 alpha_a = ln(2)/Ta
 alpha_b = ln(2)/Tb
 
-A0 = 1
-B0 = 0
+A0 = 1.0
+B0 = 0.0
 
-dt = 0.000001 # Size between calculations
-Ts = 7 # Total simulation time
+dt = 0.00001 # Size between calculations
+Ts = 10 # Total simulation time
 
 A = A0
 B = B0
@@ -28,16 +28,25 @@ def VB(t):
     if t==0:
         return B0
     # print(exp(-t*alpha_b))
-    return (A0 - VA(t) + B0)*exp(-t*alpha_b) # + (A0 - VA(t))*exp(t*(alpha_b + alpha_a))
+    # return (A0 - VA(t) + B0)*exp(-t*alpha_b) # + (A0 - VA(t))*exp(t*(alpha_b + alpha_a))
+    return B0*exp(-t*alpha_b) - A0*exp(-t*alpha_a) + A0*exp(-t*alpha_a*alpha_b)
 
-while t<Ts+dt:
+def V(t):
+    return A0*exp(-t*(alpha_a*alpha_b)) + B0*exp(-t*alpha_b)
+
+i=0
+
+while i<(Ts/dt+1):
     # leaks from A
-    A_leak = A*(1-exp(-dt*alpha_a))
-    B_leak = B*(1-exp(-dt*alpha_b))
-    t = round(t/dt)*dt
-    if t == int(t):
-        print("%16f %16f %16f %16f %16f %16f %16f %16f %16f %16f %16f" % (t, A, VA(t), B, VB(t), A_leak, VA(t) - VA(t+dt), B_leak - A_leak, VB(t) - VB(t+dt), B - VB(t), exp(-t*(alpha_b))*(1-exp(-t*(alpha_a)))  ))
-    A -= A_leak
+    # A_leak = A*(1-exp(-alpha_a*dt))
+    # B_leak = B*(1-exp(-alpha_b*dt))
+    A_leak = A*alpha_a*dt
+    B_leak = B*alpha_b*dt
+    if i%round(1.0/dt/10.0)==0:
+        # print("%16f %16f %16f %16f %16f %16f" % (t, A, B, A+B, V(t), A + B - V(t)))
+        print("%16f, %16f, %16f, %16f, %16f, %16f" % (t, A, B, A+B, V(t), A + B - V(t)))
     B += A_leak
+    A -= A_leak
     B -= B_leak
-    t+=dt    
+    t+=dt
+    i+=1
